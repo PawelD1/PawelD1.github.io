acd0bdede744fad35eeafe526881ee516a6eeeb3
LENOVO-PC\Paweł <pawel.dz.m@interia.pl>
2019-03-27 10:11:19 +0100

    Aktualization files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://arcane-thicket-98169.herokuapp.com/v1/task/';
+  const apiRoot = 'https://blooming-waters-81329.herokuapp.com/v1/task/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
